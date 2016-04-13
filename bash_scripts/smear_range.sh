42fadcd2422cb53f3e64f6757a3cd2b4964a5ccd
James Waterfield <jw419@sussex.ac.uk>
2016-04-13 11:04:13 +0100

    Makes sure were getting the right hdf5

--- bash_scripts/smear_range.sh
+++ bash_scripts/smear_range.sh
@@ -1,9 +1,11 @@
 dest="/lustre/scratch/epp/neutrino/snoplus/echidna_env27/bash_scripts/tmp"
 base="/lustre/scratch/epp/neutrino/snoplus/production/share/5.0.2/"
-#base="/lustre/scratch/epp/neutrino/snoplus/production/hdf5_spectra_v0.3/5.0.2/"
-#for x in  $(find $base | grep Summed | grep "mc_sum.hdf5$");do
 for x in  $(find $base | grep "mc_sum.hdf5$");do
-bg_dir=`dirname $x` 
+if [[ $x == *"Summed/"* ]]
+then
+    continue;
+fi
+bg_dir=`dirname $x`; 
 for i in `seq 180 220`; do
 cat <<EOT > $dest/${x##*/}_smear${i}ly.sh
 #$ -S /bin/bash
